17b25a27266942cfdc4838778b3d25bbb5aacd80
hhn <1761293315@qq.com>
2020-01-02 19:07:45 +0800

    版本1.0.1

--- src/module-hmmm/pages/questions-new.vue
+++ src/module-hmmm/pages/questions-new.vue
@@ -50,17 +50,20 @@
         <el-form-item  style="width:80%;height:480px" prop='question' label-width="100px" label="题干">
           <quill-editor v-model="formDate.question" style="height:300px"  type="textarea" :rows="2" placeholder="请输入内容"></quill-editor>
         </el-form-item>
-        <el-form-item style="width:80%;" label-width="100px" label="选项(以下选中的选项为正确答案)"></el-form-item>
-        <el-form-item
-        style="width:80%;" label-width="100px"
-        prop='options'
-          v-for="(time, index) in formDate.options"
-          :label="xuanxiang[index]"
-          :key="index"
-        >
-    <el-input v-model="time.title" style="width:300px; margin-right:10px"></el-input><el-button @click.prevent="removeDomain(time)">删除</el-button>
-  </el-form-item>
-  <el-button style="margin-left:100px;margin-bottom:10px" @click="addDomain" type="primary">+新增选项</el-button>
+        <template v-if="formDate.questionType!='3'">
+        <el-form-item  label-width="100px" label="选项(以下选中的选项为正确答案)"></el-form-item>
+        <el-form-item style="width:80%;" label-width="50px" prop='options' v-for="(item, index) in formDate.options"
+        :label="xuanxiang[index]" :key="index">
+        <el-radio v-if="formDate.questionType == '1'" v-model="item.isRight" :label="1" @change="danxuan(index)">{{item.img}}</el-radio>
+        <el-checkbox style="margin-right:10px" v-if="formDate.questionType == '2'"
+        v-model="item.isRight"
+        :true-label="1"
+        :false-label="0"
+        ></el-checkbox>
+        <el-input v-model="item.title" style="width:280px; margin-right:10px"></el-input><el-button @click.prevent="removeDomain(item)">删除</el-button>
+        </el-form-item>
+        <el-button style="margin-left:100px;margin-bottom:10px" @click="addDomain" type="primary">+新增选项</el-button>
+        </template>
         <el-form-item  style="width:80%;" label-width="100px" label="解析视频" prop='videoURL'>
           <el-input  v-model='formDate.videoURL'  placeholder="请输入内容"></el-input>
         </el-form-item>
@@ -81,12 +84,12 @@
 </template>
 
 <script>
-import {list as subjectlist} from '../../api/hmmm/subjects'
-import {list as cataloguelist} from '../../api/hmmm/directorys'
-import {list as enterpriselist} from '../../api/hmmm/companys'
-import {provinces as citysdatas,citys as cityslist} from '../../api/hmmm/citys'
-import {direction} from '../../api/hmmm/constants'
-import {add} from '../../api/hmmm/questions'
+import {list as subjectlist} from '../../api/hmmm/subjects' //学科
+import {list as cataloguelist} from '../../api/hmmm/directorys' //目录
+import {list as enterpriselist} from '../../api/hmmm/companys' //企业
+import {provinces as citysdatas,citys as cityslist} from '../../api/hmmm/citys' //城市 区
+import {direction} from '../../api/hmmm/constants' //方向
+import {add} from '../../api/hmmm/questions'//增加
 import quillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
@@ -116,7 +119,7 @@ export default {
         answer:null,//答案解析
         remarks:null,//题目备注
         tags:null,//式题标签
-        options:[{ title: '',code:'',img:'',isRight:true}],
+        options:[],//选项
       },
       regulation: {
       options:[{ required: true, message: '内容不能为空'}],
@@ -128,44 +131,54 @@ export default {
       enterpriseID: [{ required: true, message: '企业不能为空' }],
       questionType: [{required: true, message: '题型不能为空'}],
       difficulty: [{required: true, message: '难度不能为空'}],
-      question: [{required: true, message: '题干不能为空'},{min: 5, max: 30, message: '题干长度需要在5到30字符之间'}],
-      videoURL:[{required: true, message: '解析视频不能为空'},{min: 5,max: 30, message: '解析视频长度需要在5到30字符之间'}],
-      answer:[{required:true,message: '答案解析不能为空'},{min: 5,max: 30, message: '答案解析长度需要在5到30字符之间'}],
-      remarks:[{required:true,message:'题目备注不能为空'},{min: 5,max: 30, message: '题目备注长度需要在5到30字符之间'}],
-      tags:[{required:true,message:'式题标签不能为空'},{min: 5, max: 30, message: '式题标签长度需要在5到30字符之间'}]
+      question: [{required: true, message: '题干不能为空'}],
+      videoURL:[{required: true, message: '解析视频不能为空'}],
+      answer:[{required:true,message: '答案解析不能为空'}],
+      remarks:[{required:true,message:'题目备注不能为空'}],
+      tags:[{required:true,message:'式题标签不能为空'}]
       }
     };
   },
   components:('quill-editor', quillEditor),
   methods:{
-    removeDomain(item) {
+    danxuan(index){
+        let options = this.formDate.options.map((item, i) => {
+        i === index ? (item.isRight = 1) : (item.isRight = 0);
+        return item;
+      });
+      console.log(options);
+    },
+    removeDomain(item) { //删除选项
         var index = this.formDate.options.indexOf(item)
         if (index !== -1) {
           this.formDate.options.splice(index, 1)
         }
       },
-      addDomain() {
+      addDomain() { //增加选项
         this.formDate.options.push({
-          value: '',
-          key: Date.now()
+           title:'',
+           code:'',
+           img:'',
+           isRight: false
         });
+        debugger
       },
-    resetForm(checkout) {
+    resetForm(checkout) { //重置
         this.$refs[checkout].resetFields();
       },
     cityss(value){ //点击获取省份
     this.province = cityslist(value)
     },
-    increase(){
+    increase(){  //增加面试题
        this.$refs.checkout.validate((isOK)=>{
          console.log(this.formDate.options)
         add(this.formDate).then(res=>{
-          console.log(res);
+          console.log(res.config.data);
           this.$message({
            message: '添加成功',
             type: 'success'
           });
-          // this.$router.push('/questions/list')
+          this.$router.push('/questions/list')
        })
       })
     }
@@ -177,16 +190,16 @@ export default {
   },
   mounted(){  //文档被挂载后
     this.direction = direction
-    this.citys =  citysdatas()
+    this.citys =  citysdatas() //获取城市
   },
   created(){
-    subjectlist().then(res =>{
+    subjectlist().then(res =>{ //获取学科
       this.subject = res.data.items
     }),
-    cataloguelist().then(res =>{
+    cataloguelist().then(res =>{ //获取目录
       this.catalogue = res.data.items
     }),
-    enterpriselist().then(res =>{
+    enterpriselist().then(res =>{ //获取企业
       this.enterprise = res.data.items
     })
   }
